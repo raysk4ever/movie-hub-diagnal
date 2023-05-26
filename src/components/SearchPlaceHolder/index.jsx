9bb77f99160c89512cf2c579d12383c6c181d84a
Ravi <raysk7161@gmail.com>
2023-05-26 21:26:34 +0530

    added search placeholder component

---
+++ src/components/SearchPlaceHolder/index.jsx
@@ -0,0 +1,16 @@
+import Image from "../Image"
+import { Container } from "./styled"
+
+/**
+ * Responsible for rendering Search Placeholder
+ */
+const SearchPlaceHolder = () => {
+  return (
+    <Container>
+      <Image src='/favicon.ico' />
+      <h1>Get Your Dream Movie Here</h1>
+    </Container>
+  )
+}
+export default SearchPlaceHolder
+
